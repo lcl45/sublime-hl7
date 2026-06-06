d30aaca24d7c905a146537841ea352f12101239f
lcl45 <niccolo.oro@gmail.com>
2026-06-06 08:57:50 +0200

    Refactored syntax application

--- entrypoint.py
+++ entrypoint.py
@@ -5,33 +5,38 @@ from .hl7.hl7_parser import is_parseable_hl7
 
 
 class Hl7StatusListener(sublime_plugin.EventListener):
-    def on_selection_modified_async(self, view):
-        """called when selecting things inside the editor"""
-
-        if not view.match_selector(0, "source.hl7"): # no need to re-check the text content here
-            return
+    def on_activated(self, view):
+        view.settings().clear_on_change("hl7_syntax_watch")
+        view.settings().add_on_change("hl7_syntax_watch", lambda: _on_syntax_changed(view))
+        file_name = view.file_name()
+        if file_name is not None and file_name.endswith(".hl7"):
+            _update_view(view)
 
     def on_modified_async(self, view):
-        """when updating text"""
-        
-        if not can_handle_view(view):
+        if not _is_hl7_candidate(view):
             return
-
-    def on_activated(self, view):        
-        """called when a view is focused. on_load seems to not be called if the file was already opened"""
-
-        if not can_handle_view(view):
-            return
+        _update_view(view)
 
 
-def can_handle_view(view):
-    text = view.substr(sublime.Region(0, view.size()))
+def _on_syntax_changed(view):
+    if view.match_selector(0, "source.hl7"):
+        _update_view(view)
 
-    if is_parseable_hl7(text):
-        view.assign_syntax("Packages/HL7/HL7.sublime-syntax")
-        view.erase_status("hl7")
+
+def _is_hl7_candidate(view):
+    if view.match_selector(0, "source.hl7"):
         return True
+    file_name = view.file_name()
+    return file_name is not None and file_name.endswith(".hl7")
+
+
+def _update_view(view):
+    text = view.substr(sublime.Region(0, view.size()))
+    if is_parseable_hl7(text):
+        if not view.match_selector(0, "source.hl7"):
+            view.assign_syntax("Packages/HL7/HL7.sublime-syntax")
+        view.erase_status("hl7")
     else:
-        view.assign_syntax("Packages/Text/Plain text.tmLanguage") # not sure about this. will this always exist?
+        if view.match_selector(0, "source.hl7"):
+            view.assign_syntax("Packages/Text/Plain text.tmLanguage")
         view.set_status("hl7", "Not a valid HL7 message")
-        return False
